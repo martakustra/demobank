49473b812ef875da2986becbd7ed36f8639ba26b
Marta Kustra <marta.katarzyna.kustra@gmail.com>
2026-06-19 14:32:46 +0200

    run script added + balance test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -52,4 +52,25 @@ test.describe('Pulpit tests', () => {
       `Doładowanie wykonane! ${topupAmount},00PLN na numer ${receiverId}`,
     );
   });
+
+  test('correct balance after successful mobile top-up', async ({ page }) => {
+    // Arrange
+    const receiverId = '500 xxx xxx';
+    const topupAmount = '40';
+    const expectedMessage = `Doładowanie wykonane! ${topupAmount},00PLN na numer ${receiverId}`;
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(topupAmount);
+
+    // Act
+    await page.locator('#widget_1_topup_receiver').selectOption(receiverId);
+    await page.locator('#widget_1_topup_amount').fill(topupAmount);
+    await page.locator('#uniform-widget_1_topup_agreement > span').click();
+
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+    await page.waitForLoadState('domcontentloaded');
+
+    // Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
+  });
 });
