258e24f5942273ec116f4a0d13b89258b9932c92
Marta Kustra <marta.katarzyna.kustra@gmail.com>
2026-06-22 17:33:44 +0200

    POB paymentPage

---
+++ pages/payment.page.ts
@@ -0,0 +1,21 @@
+import { Locator, Page } from '@playwright/test';
+
+export class PaymentPage {
+  transferReceiver: Locator;
+  transferAccount: Locator;
+  transferAmount: Locator;
+  doTransfer: Locator;
+  closeButton: Locator;
+  showMessage: Locator;
+
+  constructor(private page: Page) {
+    this.transferReceiver = this.page.getByTestId('transfer_receiver');
+    this.transferAccount = this.page.getByTestId('form_account_to');
+    this.transferAmount = this.page.getByTestId('form_amount');
+    this.doTransfer = this.page.getByRole('button', {
+      name: 'wykonaj przelew',
+    });
+    this.closeButton = this.page.getByTestId('close-button');
+    this.showMessage = this.page.locator('#show_messages');
+  }
+}
--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { loginData } from '../test-data/login.data.ts';
 import { LoginPage } from '../pages/login.page.ts';
+import { PaymentPage } from '../pages/payment.page.ts';
 
 test.describe('Payment tests', () => {
   test.beforeEach(async ({ page }) => {
@@ -28,13 +29,21 @@ test.describe('Payment tests', () => {
     const expectedMessage = `Przelew wykonany! ${transferAmount},00PLN dla ${transferReceiver}`;
 
     // Act
-    await page.getByTestId('transfer_receiver').fill(transferReceiver);
-    await page.getByTestId('form_account_to').fill(transferAccount);
-    await page.getByTestId('form_amount').fill(transferAmount);
-    await page.getByRole('button', { name: 'wykonaj przelew' }).click();
-    await page.getByTestId('close-button').click();
+    const paymentPage = new PaymentPage(page);
+
+    await paymentPage.transferReceiver.fill(transferReceiver);
+    await paymentPage.transferAccount.fill(transferAccount);
+    await paymentPage.transferAmount.fill(transferAmount);
+    await paymentPage.doTransfer.click();
+    await paymentPage.closeButton.click();
+
+    // await page.getByTestId('transfer_receiver').fill(transferReceiver);
+    // await page.getByTestId('form_account_to').fill(transferAccount);
+    // await page.getByTestId('form_amount').fill(transferAmount);
+    // await page.getByRole('button', { name: 'wykonaj przelew' }).click();
+    // await page.getByTestId('close-button').click();
 
     // Assert
-    await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
+    await expect(paymentPage.showMessage).toHaveText(expectedMessage);
   });
 });
